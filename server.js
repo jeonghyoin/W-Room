a85de1482c55218b0edc1a28d2c8877bca09b1b5
jeonghyoin <hyooin.jeong@gmail.com>
2019-12-16 17:46:47 +0900

    라우터 설정

---
+++ server.js
@@ -0,0 +1,31 @@
+//db
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'fintech'
+});
+connection.connect();
+
+//express.js
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+
+//라우터 설정
+var notice = require('./routes/notice');
+app.use('/notice', notice);
+
+//템플렛 추가
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.listen(port);
+console.log("Listening on port ", port);
