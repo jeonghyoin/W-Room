3632edb8f0a56da417c5c4cfe0e170188c1a80a5
arochoi <jchoi1211@gmail.com>
2019-12-19 00:02:36 +0900

    db

--- mysql-db.js
+++ mysql-db.js
@@ -1,13 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-   /* host     : '192.168.30.54',
+    host     : '192.168.30.54',
     user     : 'dana',
     password : 'dana1234!',
-    database : 'wroom'*/
-    host: 'localhost',
-    user: 'root',
-    password: 'jmyc1921',
-    database: 'wroom'
+    database : 'wroom'
 });
 
 module.exports = connection;
