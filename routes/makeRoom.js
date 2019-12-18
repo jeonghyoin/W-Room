73049581a7e2e42172d2ae7125e5148aa05265cf
jeonginyun <yjiv328@gmail.com>
2019-12-18 18:12:17 +0900

    모임개설완료

--- routes/makeRoom.js
+++ routes/makeRoom.js
@@ -2,16 +2,21 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../mysql-db');
 
+var jwt = require('jsonwebtoken'); // 토큰용 
+var tokenKey = "fintech123456789danahkim"; // 토큰용
+var auth = require("../lib/auth"); // 토큰용
+
 router.get('/', function(req, res) {
     res.render('makeRoom');
 });
 
-router.post('/update', function(req, res) {
+router.post('/update', auth, function(req, res) {
+    var userId = req.decoded.userId;    
     var roomName = req.body.roomName ;
     var account = req.body.account ;
 
     connection.query('INSERT INTO roomshare SET ' +
-    'roomName = ?, roomAccount = ?' , [roomName, account], function (error, results, fields) {
+    'roomName = ?, roomAccount = ?, roomLeader = ?' , [roomName, account, userId], function (error, results, fields) {
         if (error) throw error;
         console.log(results);
         res.json(1);       
