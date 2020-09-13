a6404baf9b093185b2e2612f86b476d97d6a801b
jeonghyoin <hyooin.jeong@gmail.com>
2020-09-13 15:20:31 +0900

    구성원 목록 안뜨는 오류 수정

--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,7 @@ var connection = require('../database/mysql');
 var jwt = require('jsonwebtoken'); // 토큰용 
 var tokenKey = "fintech123456789danahkim"; // 토큰용
 var auth = require("../lib/auth"); // 토큰용
+
 router.get('/', function(req, res) {
     res.render('main');
 });
@@ -15,24 +16,18 @@ router.get('/payment/form', function(req, res) {
 
 //사용자가 가지는 같은 roomID 사람들 정보를 보내기
 router.post('/', auth, function(req, res){
-    // console.log(req.body.findEmail);
     var userEmail = req.decoded.userEmail
-    var resultObject;
     var roomIDresult;
     connection.query('SELECT * FROM user WHERE email = ?', [userEmail], function (error, results, fields) { //룸아이디 검색
         if (error) throw error;
         if(results.length < 1){
-            console.log('사용자가 없습니다');
-        }
-        else {
-            roomIDresult= results[0].roomID;
-            console.log(roomIDresult);
+            console.log('사용자가 없습니다.');
+        } else {
+            roomIDresult = results[0].roomId;
+            console.log("roomIDresult " + roomIDresult);
             connection.query('SELECT * FROM user WHERE roomID = ?', [roomIDresult], function (error, results, fields){
-                console.log(results);
-                resultObject=results;
-                res.json(resultObject);
+                res.json(results);
             })
-           // 
         }
      });
  });
